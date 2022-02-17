96ec4177e02e296dc06f4878f22f6e0ee96f3765
KavingaWeerasinghe <weerasinghekavinga@gmail.com>
2022-02-17 16:01:25 +0530

    Included Rest Template bean

--- src/main/java/com/eureka/MovieCatalogService/MovieCatalogServiceApplication.java
+++ src/main/java/com/eureka/MovieCatalogService/MovieCatalogServiceApplication.java
@@ -2,9 +2,20 @@ package com.eureka.MovieCatalogService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MovieCatalogServiceApplication {
+
+	@Bean
+	@LoadBalanced
+	public RestTemplate getRestTemplate(){
+		return new RestTemplate();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MovieCatalogServiceApplication.class, args);
